7646a4168154bfd6021d822f639dfe68711e9995
chhaya shah <chhayashah9754@gmail.com>
2026-07-14 07:30:27 +0530

    practice

---
+++ patterns/Print10.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Print10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        print2(n);
+        sc.close();
+    }
+
+    public static void print2(int n) {
+
+        for (int i = 0; i <= 2*n-1; i++) {
+            int stars = i;
+            if (i > n)
+                stars = 2 * n - i;
+            for (int j = 1; j <= stars; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}

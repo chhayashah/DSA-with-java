588c143ac0269f216093a4bc5603a46778cb49ae
chhaya shah <chhayashah9754@gmail.com>
2026-07-15 08:19:13 +0530

    fibonacciNumber

---
+++ recursion/FibonacciNumber.java
@@ -0,0 +1,16 @@
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        System.out.println(fib(5));
+    }
+
+    public static int fib(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        int ans = fib(n - 1) + fib(n - 2);
+        return ans;
+    }
+}

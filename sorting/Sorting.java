b9f5935b8a12c7c96927eb8c5b23159208651a57
chhaya shah <chhayashah9754@gmail.com>
2026-07-17 08:50:55 +0530

    selection and insertion sort

--- sorting/Sorting.java
+++ sorting/Sorting.java
@@ -13,13 +13,43 @@ public class Sorting {
                 }
             }
         }
+    }
+
+   public static void selectionSort(int arr[]){
+    int n=arr.length;
+    for(int i=0;i<n-1;i++){
+        int minIndex=i;
+        for(int j=i+1;j<n;j++){
+            if(arr[j]<arr[minIndex]){
+                minIndex=j;
+            }
+        }
+        int temp=arr[i];
+        arr[i]=arr[minIndex];
+        arr[minIndex]=temp;
+    }
    }
 
+   public static void insertionSort(int arr[]) {
+       int n = arr.length;
+       for (int i = 1; i < n; i++) {
+           int curr = i;
+           int prev = i - 1;
+           int currvalue = arr[i];
+           while (prev >= 0 && currvalue < arr[prev]) {
+               arr[prev + 1] = arr[prev];
+               prev--;
+           }
+           arr[prev + 1] = currvalue;
+       }
+   }
 
     public static void main(String[] args) {
         int arr[] = { 6, 5, 1, 3 };
 
-        bubbleSort(arr);
+        // bubbleSort(arr);
+        // selectionSort(arr);
+        insertionSort(arr);
         System.out.println("Printing the array:");
         for (int value : arr) {
             System.out.print(value+" ");

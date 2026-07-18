025cfb268c889f208a4bd143eba02db0eb061593
chhaya shah <chhayashah9754@gmail.com>
2026-07-18 23:48:36 +0530

    set and get method

--- collection freamework/Main.java
+++ collection freamework/Main.java
@@ -2,6 +2,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Iterator;
+import java.util.*;
 
 public class Main{
     public static void main(String[] args) {
@@ -42,5 +43,29 @@ public class Main{
         while (iterator.hasNext()) {
             System.out.println("Element: " + iterator.next());
         }
+
+        List<Integer> list3 = new ArrayList<>();
+        list3.add(11);
+        list3.add(12);
+        list3.add(13);
+        System.out.println(list3.get(2));
+        list3.set(0, 30);
+        System.out.println("after set:" + list3);
+
+        // toArray(): convert into array
+        Object[] arr = list3.toArray();
+        for (Object obj : arr) {
+            System.out.println(obj);
+        }
+        // contains
+        System.out.println(list3.contains(1009));
+
+        list.add(12);
+        list.add(6);
+        System.out.println("Printing Entire List: " + list);
+
+        // sort an arrayList
+        Collections.sort(list);
+        System.out.println("Printing Entire List: " + list);
     }
 }

c53a07998726adc99402e650d65c7db605a8976d
chhaya shah <chhayashah9754@gmail.com>
2026-08-24 06:58:32 +0530

    method

--- oops/Employee.java
+++ oops/Employee.java
@@ -8,15 +8,23 @@ public class Employee {
         this.id = id;
         this.salary = salary;
     }
+
+    public void displayDetails() {
+        System.out.println("Name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("Salary: " + salary);
+    }
     
     public static void main(String[] args) {
 
         Employee e1 = new Employee("chhaya", 101, 50000);
-        System.out.println(e1.name + " " + e1.id + " " + e1.salary);
+        // System.out.println(e1.name + " " + e1.id + " " + e1.salary);
+
+        e1.displayDetails();
 
         Employee e2 = new Employee("kiran", 102, 45000);
-        System.out.println(e2.name + " " + e2.id + " " + e2.salary);
+        // System.out.println(e2.name + " " + e2.id + " " + e2.salary);
 
-        
+        e2.displayDetails();
     }
 }

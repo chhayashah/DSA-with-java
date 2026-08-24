4d9f7bbbfb6157a77abea93cddd0f08c518eeb12
chhaya shah <chhayashah9754@gmail.com>
2026-08-24 06:52:19 +0530

    this

---
+++ oops/Employee.java
@@ -0,0 +1,22 @@
+public class Employee {
+    String name;
+    int id;
+    int salary;
+
+    public Employee(String name, int id, int salary) {
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
+    
+    public static void main(String[] args) {
+
+        Employee e1 = new Employee("chhaya", 101, 50000);
+        System.out.println(e1.name + " " + e1.id + " " + e1.salary);
+
+        Employee e2 = new Employee("kiran", 102, 45000);
+        System.out.println(e2.name + " " + e2.id + " " + e2.salary);
+
+        
+    }
+}

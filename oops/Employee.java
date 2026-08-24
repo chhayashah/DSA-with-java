6287a55da3009050714c1e0252b7903f83dd2bf8
chhaya shah <chhayashah9754@gmail.com>
2026-08-24 07:07:29 +0530

    Instance methods

--- oops/Employee.java
+++ oops/Employee.java
@@ -14,6 +14,10 @@ public class Employee {
         System.out.println("ID: " + id);
         System.out.println("Salary: " + salary);
     }
+
+    public int calculateAnnualSalary() {
+        return salary * 12;
+    }
     
     public static void main(String[] args) {
 
@@ -21,10 +25,12 @@ public class Employee {
         // System.out.println(e1.name + " " + e1.id + " " + e1.salary);
 
         e1.displayDetails();
+        System.out.println("Annual salary: " + e1.calculateAnnualSalary());
 
         Employee e2 = new Employee("kiran", 102, 45000);
         // System.out.println(e2.name + " " + e2.id + " " + e2.salary);
 
         e2.displayDetails();
+        System.out.println("Annual salary: " + e2.calculateAnnualSalary());
     }
 }

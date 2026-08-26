f6d0c52222f172923b47baa9f53ad4f483613476
chhaya shah <chhayashah9754@gmail.com>
2026-08-26 08:45:07 +0530

    Inheritance + Constructor + super()

--- oops/Vehicle.java
+++ oops/Vehicle.java
@@ -2,11 +2,17 @@ public class Vehicle {
     String brand;
     int speed;
 
+    public Vehicle(String brand, int speed){
+        this.brand=brand;
+        this.speed=speed;
+    }
+
     public static void main(String[] args) {
-        Car c = new Car();
-        c.brand = "yamaha";
-        c.speed = 2400;
-        c.numberOfDoors = 4;
+        // Car c = new Car();
+        // c.brand = "yamaha";
+        // c.speed = 2400;
+        // c.numberOfDoors = 4;
+        Car c = new Car("Yamaha", 240, 4);
 
         System.out.println(c.brand + " " + c.speed + " " + c.numberOfDoors);
     }
@@ -14,4 +20,9 @@ public class Vehicle {
 
 class Car extends Vehicle {
     int numberOfDoors;
+
+    public Car(String brand, int speed, int numberOfDoors) {
+        super(brand, speed);
+        this.numberOfDoors = numberOfDoors;
+    }
 }
